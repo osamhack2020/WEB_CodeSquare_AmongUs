0e56a9509202cad31b83d2337abe3ff60104b064
CirnoV <cirnov59@gmail.com>
2020-10-03 23:54:26 +0900

    개발용 mock 웹 서버 추가

---
+++ frontend/mock-server.js
@@ -0,0 +1,36 @@
+const Koa = require("koa");
+const Router = require("@koa/router");
+const logger = require("koa-logger");
+
+const app = new Koa();
+const router = new Router();
+
+router
+  .post("/user/signup", (ctx) => {
+    ctx.body = "";
+  })
+  .post("/user/signin", (ctx) => {
+    ctx.body = {
+      response: "success",
+      message: "로그인에 성공했습니다.",
+      data:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6ImlkIiwiaWF0IjoxNjAxNzEyNTM5LCJleHAiOjE2MDE3MTI1NDl9.2nw12hvcVy2LTP2PjDTbsKjN-uWKL2wPctvfvsr9-qg",
+    };
+  })
+  .post("/user/signout", (ctx) => {
+    ctx.body = "";
+  })
+  .get("/autorized", (ctx) => {
+    ctx.body = {
+      response: "success",
+      message: "성공.",
+      data: null,
+    };
+  });
+
+app.use(logger()).use(router.routes()).use(router.allowedMethods());
+
+const PORT = 8520;
+app.listen(PORT, () => {
+  console.log(`Mock listening on http://localhost:${PORT}/`);
+});
