358e193570d41c606bc8e9cd397e15a51bd8060e
CirnoV <cirnov59@gmail.com>
2020-10-03 16:43:06 +0900

    Redux core 모듈, Provider 추가

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -2,13 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "@csstools/normalize.css";
 import GlobalStyles from "./GlobalStyles";
+import rootReducer from "./modules";
 import App from "./App";
 import { Global } from "@emotion/core";
+import { createStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
+
+const store = createStore(
+  rootReducer,
+  (window as any).__REDUX_STATE__,
+  composeWithDevTools(),
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <Global styles={GlobalStyles} />
-    <App />
+    <Provider store={store}>
+      <Global styles={GlobalStyles} />
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root"),
 );
