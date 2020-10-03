3175ceb1baa262a3bb9865bbd596af5d5cc71c1f
CirnoV <cirnov59@gmail.com>
2020-10-03 13:05:31 +0900

    React 개발 환경 세팅

---
+++ web/src/App.tsx
@@ -0,0 +1,16 @@
+/** @jsx jsx */
+import { css, jsx } from "@emotion/core";
+
+function App() {
+  return (
+    <div
+      css={css`
+        font-size: 20px;
+      `}
+    >
+      Hello
+    </div>
+  );
+}
+
+export default App;
