fc91110bd535946a8ce0161ef1d5c09c5b403271
CirnoV <cirnov59@gmail.com>
2020-10-04 17:33:08 +0900

    LoginInputForm 스타일 추가

--- frontend/src/components/login/LoginInputForm.tsx
+++ frontend/src/components/login/LoginInputForm.tsx
@@ -1,5 +1,5 @@
 /** @jsx jsx */
-import { jsx } from "@emotion/core";
+import { css, jsx } from "@emotion/core";
 import styled from "@emotion/styled";
 
 const LoginInputBlock = styled.form`
@@ -31,13 +31,35 @@ export const LoginInput: React.FC<LoginInputProps> = ({
         e.preventDefault();
         onSubmit && onSubmit(value);
       }}
+      {...props}
     >
-      <div>{label}</div>
+      <div
+        css={css`
+          font-size: 14px;
+          font-style: normal;
+          font-weight: 700;
+          line-height: 20px;
+          letter-spacing: -0.02em;
+          text-align: left;
+          padding-bottom: 6px;
+        `}
+      >
+        {label}
+      </div>
       <input
+        css={css`
+          border: 1px solid #e3e3e3;
+          height: 36px;
+          border-radius: 4px;
+          :disabled {
+            color: #959da5;
+            background-color: #f3f4f6;
+          }
+        `}
         type={password ? "password" : "text"}
         value={value}
         onChange={onChange}
-        {...props}
+        disabled={disabled}
       ></input>
     </LoginInputBlock>
   );
