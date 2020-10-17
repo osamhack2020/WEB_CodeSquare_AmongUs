c36db6d96f1a28f879e53f7185961c4265baa067
CirnoV <cirnov59@gmail.com>
2020-10-17 17:26:53 +0900

    RadioButtons 컴포넌트 추가, QnaPostContainer 답변 정렬 버튼 구현

--- frontend/src/containers/qna/QnaPostContainer.tsx
+++ frontend/src/containers/qna/QnaPostContainer.tsx
@@ -1,8 +1,9 @@
 /** @jsx jsx */
 import { css, jsx } from "@emotion/core";
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import { useHistory } from "react-router-dom";
 import { OutlineButton } from "../../components/common/OutlineButton";
+import { RadioButtons } from "../../components/common/RadioButtons";
 import { QnaListWidget } from "../../components/qna/QnaListWidget";
 import { QnaPostViewer } from "./QnaPostViewer";
 
@@ -14,6 +15,13 @@ export const QnaPostContainer: React.FC = () => {
     },
     [history],
   );
+  const [sortOrder, setSortOrder] = useState("recommend");
+  const onChangeSortOrder = useCallback(
+    (order: string) => {
+      setSortOrder(order);
+    },
+    [setSortOrder],
+  );
   return (
     <div
       css={css`
@@ -21,19 +29,58 @@ export const QnaPostContainer: React.FC = () => {
         margin: 0 auto;
         padding-left: 24px;
         padding-right: 24px;
-        padding-top: 83px;
+        padding-top: 48px;
         display: flex;
       `}
     >
-      <QnaPostViewer
+      <div
         css={css`
+          display: flex;
+          flex-direction: column;
           flex: 1 1 auto;
         `}
-      />
+      >
+        <QnaPostViewer />
+        <div
+          css={css`
+            display: flex;
+            justify-content: space-between;
+          `}
+        >
+          <div
+            css={css`
+              font-size: 24px;
+              font-style: normal;
+              font-weight: 700;
+              line-height: 35px;
+              letter-spacing: -0.02em;
+              text-align: left;
+              margin-left: 52px;
+            `}
+          >
+            <span
+              css={css`
+                color: #627bff;
+              `}
+            >
+              2개
+            </span>
+            의 답변이 있습니다.
+          </div>
+          <RadioButtons
+            value={sortOrder}
+            values={["recommend", "recent"]}
+            labels={["추천순", "최신순"]}
+            onChange={onChangeSortOrder}
+          />
+        </div>
+        <QnaPostViewer answer accepted />
+        <QnaPostViewer answer />
+      </div>
       <div
         css={css`
           flex: 1 0 190px;
-          margin-top: 16px;
+          margin-top: 52px;
           margin-left: 40px;
           & > div:not(:last-child) {
             padding-bottom: 28px;
