f0c3d4c0dbce7f25bf55f8f9d9fb835d7804e055
Chanyoung Oh <ndcd607@gmail.com>
2020-10-19 14:39:56 +0000

    Add CodeSquare favicon - Issue #31
    - React 기본 파비콘을 CodeSquare 로고로 교체
    - 'react-helmet'을 적용하여 파비콘 삽입 및 모던 브라우저 파비콘 대응

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@
 import { css, jsx } from "@emotion/core";
 import { useSelector } from "react-redux";
 import { Route } from "react-router-dom";
+import { Helmet } from "react-helmet";
 import { Header } from "./components/base/Header";
 import { LoginContainer } from "./containers/auth/LoginContainer";
 import { RegisterContainer } from "./containers/auth/RegisterContainer";
@@ -22,6 +23,29 @@ const App: React.FC = () => {
         height: 100%;
       `}
     >
+      <Helmet>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#2b5797" />
+        <meta name="theme-color" content="#ffffff" />
+      </Helmet>
       <Header />
       <div
         css={css`
