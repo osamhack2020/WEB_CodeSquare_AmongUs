0a2046d3024fd8bc5b1a5aceac53d0ebe526ef49
CirnoV <cirnov59@gmail.com>
2020-10-20 00:14:53 +0900

    react-helmet 대신 react-helmet-async 적용

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 import "@csstools/normalize.css";
 import GlobalStyles from "./GlobalStyles";
 import rootReducer from "./modules";
@@ -19,10 +20,39 @@ const store = createStore(
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <Global styles={GlobalStyles} />
-        <App />
-      </BrowserRouter>
+      <HelmetProvider>
+        <BrowserRouter>
+          <Helmet>
+            <link
+              rel="apple-touch-icon"
+              sizes="180x180"
+              href="/apple-touch-icon.png"
+            />
+            <link
+              rel="icon"
+              type="image/png"
+              sizes="32x32"
+              href="/favicon-32x32.png"
+            />
+            <link
+              rel="icon"
+              type="image/png"
+              sizes="16x16"
+              href="/favicon-16x16.png"
+            />
+            <link rel="manifest" href="/site.webmanifest" />
+            <link
+              rel="mask-icon"
+              href="/safari-pinned-tab.svg"
+              color="#5bbad5"
+            />
+            <meta name="msapplication-TileColor" content="#2b5797" />
+            <meta name="theme-color" content="#ffffff" />
+          </Helmet>
+          <Global styles={GlobalStyles} />
+          <App />
+        </BrowserRouter>
+      </HelmetProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root"),
