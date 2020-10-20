deba6cbf475d6ed5846be18caab6f165a6340bd7
lijm1358 <lijm1358@gmail.com>
2020-10-20 14:24:58 +0000

    api server(for nginx conf) added

---
+++ apiserver/server.js
@@ -0,0 +1,51 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const path = require('path');
+const router = jsonServer.router(path.join(__dirname, 'db.json'));
+const middlewares = jsonServer.defaults();
+
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+const adapter = new FileSync('db.json');
+const db = low(adapter);
+
+//Set default middlewares (logger, static, cors and no-cache)
+server.use(middlewares);
+// To handle POST, PUT and PATCH you need to use a body-parser
+// You can use the one used by JSON Server
+server.use(jsonServer.bodyParser);
+
+server.post('/urlinfo', (req, res) => {
+	const name = req.body['name'] || '';
+	const addr = req.body['addr'] || '';
+
+	let urlinfo = db.get('urlinfo').value();
+
+	if(!name.length){
+		return res.status(400).json({err: 'Incorrect name'});
+	}
+	if(!addr.length){
+		return res.status(400).json({err: 'Incorrect address'});
+	}
+
+	const id = urlinfo.reduce((maxId, name) => {
+		return name.id > maxId ? name.id : maxId;
+	}, 0) + 1;
+
+	const newInfo = {
+		id: id,
+		name: name,
+		addr: addr
+	};
+
+	db.get('urlinfo').push(newInfo).write();
+
+	return res.status(201).json(newInfo);
+});
+
+server.use(router);
+
+let port = 8890;
+server.listen(port, () => {
+  console.log(`JSON Server is running, port(${port})`)
+});
