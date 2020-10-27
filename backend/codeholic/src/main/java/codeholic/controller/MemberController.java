6495c046a7c1b02f0c2afb54da93eacecefa72ec
namkyu1999 <53862866+namkyu1999@users.noreply.github.com>
2020-10-27 14:08:52 +0000

    Fix sercurity Config

--- backend/codeholic/src/main/java/codeholic/controller/MemberController.java
+++ backend/codeholic/src/main/java/codeholic/controller/MemberController.java
@@ -2,7 +2,6 @@ package codeholic.controller;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -16,7 +15,6 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
