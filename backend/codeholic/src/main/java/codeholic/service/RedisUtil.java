39c07648c490f98f7b5018a8c26c367a99a877fa
namkyu1999 <53862866+namkyu1999@users.noreply.github.com>
2020-10-29 10:22:36 +0000

    Fix redis services

--- backend/codeholic/src/main/java/codeholic/service/RedisUtil.java
+++ backend/codeholic/src/main/java/codeholic/service/RedisUtil.java
@@ -26,16 +26,14 @@ public class RedisUtil {
 
     public void setData(String key, String value){
         // 이미 데이터가 있는경우 제외
-        if(!this.getData(key).isEmpty())
-            this.deleteData(key);
+        this.deleteData(key);
         ValueOperations<String,String> valueOperations = stringRedisTemplate.opsForValue();
         valueOperations.set(key,value);
     }
 
     // 만료기간설정해서 데이터 집어넣음
     public void setDataExpire(String key,String value,long duration){
-        if(!this.getData(key).isEmpty())
-            this.deleteData(key);
+        this.deleteData(key);
         ValueOperations<String,String> valueOperations = stringRedisTemplate.opsForValue();
         Duration expireDuration = Duration.ofSeconds(duration);
         valueOperations.set(key,value,expireDuration);
