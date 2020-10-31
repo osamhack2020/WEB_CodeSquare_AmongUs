82402bcd82570a942e98e7fda29a5e027eb26ea8
namkyu1999 <53862866+namkyu1999@users.noreply.github.com>
2020-10-31 03:29:13 +0000

    Add socket test

--- backend/codeholic/src/main/java/codeholic/controller/SocketController.java
+++ backend/codeholic/src/main/java/codeholic/controller/SocketController.java
@@ -24,7 +24,7 @@ import codeholic.service.RedisUtil;
 @Controller
 @RequestMapping("vm")
 @CrossOrigin(origins = "*")
-public class SockerController {
+public class SocketController {
     private String openstackDomain="http://stack.codesquare.space";
     
     // 레디스에 토큰을 idOpenstack 으로 저장
@@ -41,7 +41,9 @@ public class SockerController {
     public SocketMessage broadCast(SocketMessage message,HttpServletResponse res) {
 
         SocketMessage result = new SocketMessage();
-
+        result.setStatus("success");
+        return result;
+        /*
         // 메세지에 데이터 넣어서 보내줘야지 -> 이거 파싱
         String username = message.getData();
         String authenticationToken = redisUtil.getData(username + "Openstack");
@@ -84,6 +86,65 @@ public class SockerController {
         result.setData("loading");
         res.setStatus(HttpServletResponse.SC_BAD_REQUEST);
         return result;
+        */
     }
     
 }
+
+/*
+@Autowired
+    RedisUtil redisUtil;
+    Logger logger = LoggerFactory.getLogger(VmController.class);
+
+    // TODO: idOpenstack의 유효시간이 지나면 어떡할 것인가? - 다시 signin 해서 받아야지 뭐...
+
+    // 이미 만든 사용자라면... 그냥 도메인에 url을 추가하면 되지 않을까?
+    // vm 삭제하기
+    @GetMapping("/create")
+    public Response createVm(HttpServletRequest httpServletRequest,HttpServletResponse res) {
+
+        Response response = new Response();
+        final String accessJwtHeader = httpServletRequest.getHeader("Authorization");
+
+        try {
+            if (accessJwtHeader == null)
+                throw new Exception();
+            String username = jwtUtil.getUsername(accessJwtHeader.substring(7));
+            String authenticationToken = redisUtil.getData(username + "Openstack");
+            if(openStackApiService.getUserInstanceCount(username, authenticationToken)!=0){
+                String tmp = openStackApiService.getUserInstanceId(username, authenticationToken);
+                openStackApiService.deleteVm(authenticationToken, tmp);
+            }
+            openStackApiService.createVm(username, authenticationToken);
+            response.setMessage("vm 생성중입니다");
+        } catch (Exception e) {
+            res.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            response.setResponse("fail");
+            response.setMessage("vm 생성실패");
+        }        
+        return response;
+    }
+    @DeleteMapping("/delete")
+    public Response deleteVm(HttpServletRequest httpServletRequest,HttpServletResponse res) {
+
+        Response response = new Response();
+        final String accessJwtHeader = httpServletRequest.getHeader("Authorization");
+
+        // 이전에 만든적 있다면 빠꾸하는 로직이 필요하다...
+
+        try {
+            if (accessJwtHeader == null)
+                throw new Exception();
+            String username = jwtUtil.getUsername(accessJwtHeader.substring(7));
+            String authenticationToken = redisUtil.getData(username + "Openstack");
+            String instanceId = openStackApiService.getUserInstanceId(username, authenticationToken);
+            openStackApiService.deleteVm(authenticationToken, instanceId);
+            response.setMessage("vm 삭제 성공");
+        } catch (Exception e) {
+            res.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            response.setResponse("fail");
+            response.setMessage("vm 삭제 실패");
+        }        
+        return response;
+    }
+    */
